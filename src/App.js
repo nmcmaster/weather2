1dbac27f50196947e784c8b23bf3b7f519079194
Nicholas McMaster <nmcmaster@gmail.com>
2019-08-24 19:49:28 -0400

    made button disable on invalid zip format

--- src/App.js
+++ src/App.js
@@ -9,23 +9,50 @@ const postcode = require("postcode-validator");
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { zip: "" };
+    this.state = { zip: "", validFormat: false };
     this.handleChange = this.handleChange.bind(this);
     this.submit = this.submit.bind(this);
   }
   handleChange(event) {
     this.setState({ zip: event.target.value });
+    if (postcode.validate(event.target.value, "US")) {
+      this.setState({ validFormat: true });
+    } else {
+      this.setState({ validFormat: false });
+    }
   }
   submit() {
-    if (postcode.validate(this.state.zip, "US")) {
-      alert("it is valid");
-    } else {
-      alert("it iss not valid");
-    }
+    alert("this worked");
   }
 
   render() {
-    let valid = <div className="text-white">Please Enter Zip Code</div>;
+    let valid = (
+      <div className="text-white pb-2 pt-1">Please Enter Zip Code</div>
+    );
+    let zip = this.state.zip;
+    if (zip.length > 4 && postcode.validate(zip, "US")) {
+      valid = (
+        <div className="text-blue-400 pb-2 pt-1">
+          valid numeric format zip code!
+        </div>
+      );
+    } else if (zip.length > 4 && !postcode.validate(zip, "US")) {
+      valid = (
+        <div className="text-red-400 pb-2 pt-1 ">invalid zip code format</div>
+      );
+    }
+    let button = (
+      <Button variant="contained" disabled>
+        <span className="normal-case">Find Weather</span>
+      </Button>
+    );
+    if (this.state.validFormat === true) {
+      button = (
+        <Button variant="contained" onClick={this.submit}>
+          <span className="normal-case">Find Weather</span>
+        </Button>
+      );
+    }
 
     return (
       <div
@@ -44,11 +71,8 @@ class App extends React.Component {
             onChange={this.handleChange}
           />
           {valid}
-          <div>
-            <Button variant="contained" onClick={this.submit}>
-              <span className="normal-case">Find Weather</span>
-            </Button>
-          </div>
+          {button}
+          <div></div>
         </div>
       </div>
     );
@@ -56,12 +80,3 @@ class App extends React.Component {
 }
 
 export default App;
-//
-// let zip = this.state.zip;
-// if (zip.length > 4 && postcode.validate(zip, "US")) {
-//   valid = <div className="text-blue-400">valid zip code!</div>;
-// } else if (zip.length > 4 && !postcode.validate(zip, "US")) {
-//   valid = <div className="text-red-400">invalid zip code</div>;
-// } else {
-//   valid = <div className="text-white">invalid zip code</div>;
-// }
