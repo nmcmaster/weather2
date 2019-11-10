64ee1b6470ee991c87994f3d29ff7f51848cc1cf
Nicholas McMaster <nmcmaster@gmail.com>
2019-11-10 17:34:11 -0500

    began data hooks fetch implementation

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,7 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Forecast(props) {
-  return <div>it workds</div>
+  const [forecast, setForecast] = useState("");
+  const [data, setData] = useState({ hits: [] });
+
+  const apiKey = "db51a3b4c19848e3902191949192004";
+  const forecastUrl = "https://api.apixu.com/v1/forecast.json?key=";
+  let zipcode = 11385;
+  let weiruch = "https://hn.algolia.com/api/v1/search?query=redux";
+
+  const urlToFetch = forecastUrl + apiKey + "&q=" + zipcode;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        "https://hn.algolia.com/api/v1/search?query=redux"
+      );
+      setData(result.data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {" "}
+      <ul>
+        {data.hits.map(item => (
+          <li key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Forecast;
