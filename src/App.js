27fde1bd82e87a5cb10367cc2b3c732ee86adc44
Nicholas McMaster <nmcmaster@gmail.com>
2019-11-10 13:22:03 -0500

    made view state switcher in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import ZipEntry from "./components/ZipEntry";
 
 function App() {
-  return <ZipEntry />;
+  const [view, setView] = useState("ZipEntry");
+  if (view === "ZipEntry") {
+    return <ZipEntry />;
+  }
 }
 
 export default App;
